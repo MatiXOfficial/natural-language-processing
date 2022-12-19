c78ffddac144c41d59bbbcd95771b7d9ec27b790
Mateusz Kocot <mateuszkocot99@gmail.com>
2022-12-19 12:46:17 +0100

    Completed lab8

---
+++ lab8/questions.jl
@@ -0,0 +1,21 @@
+{"_id": "1345", "text": "Czy źródła finansowania partii politycznych mogą być niejawne?"}
+{"_id": "1346", "text": "Czy wnuki przyjęte na wychowanie mają prawo do renty po zmarłych rodzicach?"}
+{"_id": "1347", "text": "Kiedy kurator składa wniosek o ogłoszenie upadłości osoby prawnej?"}
+{"_id": "1348", "text": "Czy dawka promieniowania jonizującego pochodzącego ze źródeł naturalnych może przekroczyć dawkę graniczną?"}
+{"_id": "1349", "text": "Czy dawka promieniowania jonizującego pochodzącego ze źródeł naturalnych może przekroczyć dawkę graniczną?"}
+{"_id": "1350", "text": "Jaki tytuł ma obecnie Wyższa Szkoła Inżynierska w Koszalinie?"}
+{"_id": "1351", "text": "Czy wydanie dyspozycji wyjazdu/wylotu zespołu ratownictwa medycznego w razie konieczności użycia dodatkowych jednostek systemu rodzi roszczenia finansowe?"}
+{"_id": "1352", "text": "Co kontroluje okręgowa komisja rewizyjna?"}
+{"_id": "1353", "text": "Czy ktoś, kto przewozi materiał jądrowy bez właściwego zabezpeczenia podlega karze ograniczenia wolności?"}
+{"_id": "1354", "text": "Czy producent butelek miarowych może przekazać opis przyjętego systemu kontroli wewnętrznej po rozpoczęciu produkcji?"}
+{"_id": "1355", "text": "Czy wyborca niepełnosprawny jest dopisywany do spisu wyborców?"}
+{"_id": "1356", "text": "Kto wydaje decyzje w sprawach udzielania i pozbawiania azylu w RP?"}
+{"_id": "1357", "text": "Kto wydaje decyzje w sprawach udzielania i pozbawiania azylu w RP?"}
+{"_id": "1358", "text": "Jak wysoka jest kara dla uchylającego się od służby funkcjonariusza BOR?"}
+{"_id": "1359", "text": "Na czyj wniosek inne jednostki organizacyjne mogą uzyskać osobowość prawną?"}
+{"_id": "1360", "text": "Czy strona może cofnąć odwołanie przed wydaniem decyzji przez organ odwoławczy?"}
+{"_id": "1361", "text": "Kto może określić, w drodze rozporządzenia, dodatkowe kryteria akredytacyjne?"}
+{"_id": "1362", "text": "Na kim spoczywa obowiązek prowadzenia kontroli źródeł promieniowania jonizującego?"}
+{"_id": "1363", "text": "Co może ustalić organ ochrony środowiska w drodze decyzji?"}
+{"_id": "1364", "text": "Kto określi w drodze rozporządzenia rodzaje preparatów i ich ilości jakie mogą posiadać zakłady opieki?"}
+{"_id": "1365", "text": "Kto wykonuje zadania wojewody w zakresie ochrony przyrody na terenie parku narodowego?"}
